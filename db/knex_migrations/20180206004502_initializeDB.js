4a65a8ec4f53c194a5658912c4bd5d7ab8c6431a
yanipo5 <[yip528@gmail.com]>
2018-02-06 14:25:30 +0200

    fixes error on server mysql error

--- db/knex_migrations/20180206004502_initializeDB.js
+++ db/knex_migrations/20180206004502_initializeDB.js
@@ -14,7 +14,7 @@ exports.up = function (knex, Promise) {
             table.enu(booksTable.genre, GENERES).notNullable();
             table.integer(booksTable.price).notNullable();
             table.timestamp(booksTable.created_at).defaultTo(knex.fn.now());
-            table.timestamp(booksTable.updated_at).defaultTo(knex.fn.now());
+            table.timestamp(booksTable.updated_at);
         })
 };
 
