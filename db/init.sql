b1d37c4cf9d659076c285475c0b481e7b78f709d
yanipo5 <[yip528@gmail.com]>
2018-02-07 19:58:29 +0200

    adding heberew support

--- db/init.sql
+++ db/init.sql
@@ -2,6 +2,9 @@
 DROP DATABASE IF EXISTS `bookstore`;
 CREATE DATABASE IF NOT EXISTS `book_app_db`;
 
+ALTER DATABASE `book_app_db`CHARACTER SET utf8;
+ALTER DATABASE `book_app_db`COLLATE utf8_general_ci;
+
 DROP USER IF EXISTS 'book_app_sys'@'localhost';
 create USER 'book_app_sys'@'localhost' IDENTIFIED BY 'book_app_sys';
 GRANT ALL PRIVILEGES ON `book_app_db` . * TO 'book_app_sys'@'localhost';
