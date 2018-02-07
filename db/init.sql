6aff1fd223d3770166ca76a3d0a59a414eb32e5f
yanipo5 <[yip528@gmail.com]>
2018-02-07 15:03:10 +0200

    init db

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+# Implicitly create the user and then drop the user.
+DROP DATABASE IF EXISTS `bookstore`;
+CREATE DATABASE IF NOT EXISTS `book_app_db`;
+
+DROP USER IF EXISTS 'spark'@'localhost';
+create USER 'book_app_sys'@'localhost' IDENTIFIED BY 'book_app_sys';
+GRANT ALL PRIVILEGES ON `book_app_db` . * TO 'book_app_sys'@'localhost';
