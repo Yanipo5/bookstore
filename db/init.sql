a337896d5633f3d02e1931df099abf30597c7124
yanipo5 <[yip528@gmail.com]>
2018-02-07 21:30:30 +0200

    spaces

--- db/init.sql
+++ db/init.sql
@@ -2,8 +2,8 @@
 DROP DATABASE IF EXISTS `book_app_db`;
 CREATE DATABASE IF NOT EXISTS `book_app_db`;
 
-ALTER DATABASE `book_app_db`CHARACTER SET utf8;
-ALTER DATABASE `book_app_db`COLLATE utf8_general_ci;
+ALTER DATABASE `book_app_db` CHARACTER SET utf8;
+ALTER DATABASE `book_app_db` COLLATE utf8_general_ci;
 
 DROP USER IF EXISTS 'book_app_sys'@'localhost';
 create USER 'book_app_sys'@'localhost' IDENTIFIED BY 'book_app_sys';
