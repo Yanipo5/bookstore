5f41b9b584d770b5eecac5c6ecbb347e6ccb7582
yanipo5 <[yip528@gmail.com]>
2018-02-13 20:16:15 +0200

    removed cross origin acceptacne

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-// access setting
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
