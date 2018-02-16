4603bd8a529ac31f7d45b37483a28527f85707ec
yanipo5 <[yip528@gmail.com]>
2018-02-16 10:01:55 +0200

    book directive

--- public/js/directives/book.js
+++ public/js/directives/book.js
@@ -1,14 +1,19 @@
 app.directive("book", () => {
   return {
+      scope:{
+          book : "=directive",
+          edit : "&edit",
+          remove : "&remove",
+      },
     template: `
         <div class="book" id='b{{book.id}}'>
                 <p class="bookTitle">{{book.title}}</p>
-                <span ng-click=setEditModal(book.id) class="btn btn-outline-dark btn-sm" data-toggle="modal" data-target="#BookModal">
+                <span ng-click=edit() class="btn btn-outline-dark btn-sm" data-toggle="modal" data-target="#BookModal">
                     <svg class="icon">
                         <use xlink:href="node_modules/bytesize-icons/dist/bytesize-inline.svg#i-edit"></use>
                     </svg>
                     edit</span>
-                <span ng-click=promptDelete(book.id) class="btn btn-outline-danger btn-sm">
+                <span ng-click=remove() class="btn btn-outline-danger btn-sm">
                     <svg class="icon">
                         <use xlink:href="node_modules/bytesize-icons/dist/bytesize-inline.svg#i-trash"></use>
                     </svg>
